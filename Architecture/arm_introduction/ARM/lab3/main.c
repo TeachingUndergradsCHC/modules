7e8a56af1790cf48320fee674b73a981afdb8902
Phil Schielke <philip.schielke@concordia.edu>
2020-10-30 14:39:22 -0500

    Added C version of function, changed number of array elements, and updated comments

--- Architecture/arm_introduction/ARM/lab3/main.c
+++ Architecture/arm_introduction/ARM/lab3/main.c
@@ -1,17 +1,22 @@
 // This is lab3 of the Introduction to ARM module.
 // This code sets up three arrays of bytes (unigned characters)
 // and sums the first two arrays putting the result in the
-// third array.  Two calls are made to functions implemented
+// third array.  The first call is made to a C function that adds
+// the arrays element by element.  Then, two calls are made to functions implemented
 // in ARM assembly.  The first is given and is is a simple
 // loop that adds each element of the array individually
 // putting the sums into the third array.  The second
 // is to be developed by the students to use Neon instructions
-// to speed up this operation.  Timeing code is provided
+// to speed up this operation.  Timing code is provided
 // to time each of these.
+
 #include <stdio.h>
+#include <time.h>
 
-#define SIZE 1024 // Size of arrays to manipulate
+#define SIZE 16384 // Size of arrays to manipulate, should be a power of 2
+                   // at least 128
 
+// put some initial values in x, and y, and set z array to all 0
 void initialize_arrays(unsigned char *x, unsigned char *y, unsigned char *z)
 {
   int i;
@@ -22,15 +27,8 @@ void initialize_arrays(unsigned char *x, unsigned char *y, unsigned char *z)
   return;
 }
 
-void add_arrays_linear(unsigned char *x, unsigned char *y, unsigned char *z, int size)
-{
-  int i;
-  for (i = 0; i < size; i++)
-    z[i] = x[i] + y[i];
-  return;
-}
-
-void add_arrays_neon(unsigned char *x, unsigned char *y, unsigned char *z, int size)
+// This is a C version of adding the arrays element by element.
+void add_arrays_linear_c(unsigned char *x, unsigned char *y, unsigned char *z, int size)
 {
   int i;
   for (i = 0; i < size; i++)
@@ -43,14 +41,24 @@ int main()
   unsigned char a[SIZE],b[SIZE],c[SIZE];
   
   initialize_arrays(a, b, c);
-  // timing code start
+  
+  clock_t CPU_time_1 = clock();
+  add_arrays_linear_c(a, b, c, SIZE);
+  clock_t CPU_time_2 = clock();
+  printf("\nTime taken linear C: %ld\n",CPU_time_2 - CPU_time_1);
+  printf("%i %i %i %i %i %i %i %i %i %i\n",c[0],c[1],c[2],c[3],c[10],c[11],c[63],c[64],c[65],c[66]);
+
+  clock_t CPU_time_3 = clock();
   add_arrays_linear(a, b, c, SIZE);
-  // time code end
-  printf("%i %i %i %i %i %i %i %i %i %i\n",c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7],c[8],c[9]);
+  clock_t CPU_time_4 = clock();
+  printf("\nTime taken linear asm: %ld\n",CPU_time_4 - CPU_time_3);
+  printf("%i %i %i %i %i %i %i %i %i %i\n",c[0],c[1],c[2],c[3],c[10],c[11],c[63],c[64],c[65],c[66]);
 
   initialize_arrays(a, b, c);
-  // timing code start
+  clock_t CPU_time_5 = clock();
   add_arrays_neon(a, b, c, SIZE);
-  // timing code end
-  printf("%i %i %i %i %i %i %i %i %i %i\n",c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7],c[8],c[9]);
+  clock_t CPU_time_6 = clock();
+  printf("\nTime taken Neon ASM: %ld\n",CPU_time_6 - CPU_time_5);
+  printf("%i %i %i %i %i %i %i %i %i %i\n",c[0],c[1],c[2],c[3],c[10],c[11],c[63],c[64],c[65],c[66]);
+
 }
