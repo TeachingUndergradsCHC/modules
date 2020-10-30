ad16be32a9a52259cd7302d450ae32847958ae26
Phil Schielke <philip.schielke@concordia.edu>
2020-10-30 14:39:47 -0500

    Striped Neon code out so students can write it

--- Architecture/arm_introduction/ARM/lab3/add_arrays_neon.s
+++ Architecture/arm_introduction/ARM/lab3/add_arrays_neon.s
@@ -1,27 +1,29 @@
-
+        .global add_arrays_neon
+	.text
+	
 add_arrays_neon:
 	@ R0 contains the pointer to array x
 	@ R1 contains the pointer to array y
 	@ R2 contains the pointer to array z
 	@ R3 contains the size
 	PUSH {LR}
-	PUSH {R4,D0-D2}   @ Save content of these registers
+	PUSH {R4}   @ Save content of these registers
 	MOV R4, #0        @ Loop index variable
 loop_head:
 	CMP R4, R3
 	BGE loop_exit
 loop_body:
 	@  Load 8 elements of array x into 64 bit register
-	VLD1.64 {D0}, [R0]!
+
 	@  Load 8 elements of array y into 64 bit register
-	VLD1.64 {D1}, [R1]!	
+
 	@  Perform an 8x8 SIMD add on these two registers
-	VADD.U8 D2, D0, D1
+
 	@  Store the result into array z
-	VSTR1.64 {D2}, [R2]!
-	
-	ADD R4, R4, #8
+
+	@  Update the loop index variable
+
 	BAL loop_head
 loop_exit:
-	POP {R4,D0-D2}    @ Restore content of these registers
-	POP {SP}          @ Return
+	POP {R4}    @ Restore content of these registers
+	POP {PC}          @ Return
