dbe57a430d532f25bdbd8d4f1de20969144a68e9
Apan Qasem <aq10@txstate.edu>
2021-07-08 16:28:20 -0500

    html versions of D2 demos

--- Programming/openmp/code/hello.c
+++ Programming/openmp/code/hello.c
@@ -1,38 +1,20 @@
-/* 
- * This program is used in the Hello World in OpenMP Demo 
- *  @author Apan Qasem 
- */
-
 #include<stdio.h>
+#include<stdlib.h>
 #include<omp.h>
 
-void say_hello(unsigned n) {
-  int i = 0;
-  for (i = 0; i < n; i++)  
-    printf("Hello World!\n");
-  return;
-}
+int main(int argc, char* argv[]) {
+  int num_threads;
+  if (argc <= 1)
+    num_threads = 1;
+  else
+    num_threads = atoi(argv[1]);
 
-void say_goodbye(unsigned n) {
-  int i = 0;
-  for (i = 0; i < n; i++)  
-    printf("Goodbye World!\n");
-  return;
-}
+  omp_set_num_threads(num_threads);
 
-int main() {
-
-  omp_set_num_threads(4);
-
-  #pragma omp parallel
-  {
-
-    //  uncomment following lines to demonstrate asynchronous execution of functions 
-    //  say_hello(4);
-    //  say_goodbye(4);
-
+#pragma omp parallel 
+  { 
     int ID = omp_get_thread_num();
-    printf("Hello World from %d!\n", ID);
+    printf("Hello World from %d\n", ID);
     printf("Goodbye World from %d!\n", ID);
   }
   return 0;
